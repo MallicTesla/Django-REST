883c7a107ea793186c0ab57aeacf40151cd68b2d
Mallic Tesla <el_mallic@hotmail.com>
2023-09-13 19:49:44 -0300

    hotra forma de entregar el json

--- Tienda/usuarios/api/api.py
+++ Tienda/usuarios/api/api.py
@@ -1,14 +1,31 @@
 # este archivo rempasa al views
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from usuarios.models import Usuario
 from usuarios.api.serializers import UsuarioSerializers
 
-class UsuarioAPIView (APIView):
-    def get (self,request):
+
+# class UsuarioAPIView (APIView):
+#     def get (self,request):
+#         usuarios=Usuario.objects.all ()
+#         #   cuando queres serealizar un listado tenes que agregarle (many = True) para que sepa que es mas de uno
+#         usuarios_serializer = UsuarioSerializers (usuarios, many = True)
+
+#         # para pasar el json se tiene que agregar (.data) al final de de la info serealizada
+#         return Response (usuarios_serializer.data)
+
+#   esto hace lo mismo que la clase UsuarioAPIView
+#   al decorador se le pasa que metodos se le van a permiter que tenga la funcion
+@api_view (["GET","POST"])
+def usuario_api_view (request):
+    if request.method == "GET":
         usuarios=Usuario.objects.all ()
         #   cuando queres serealizar un listado tenes que agregarle (many = True) para que sepa que es mas de uno
         usuarios_serializer = UsuarioSerializers (usuarios, many = True)
 
-        # para pasar el jeison se tiene que agregar (.data) al final de de la info serealizada
+        # para pasar el json se tiene que agregar (.data) al final de de la info serealizada
         return Response (usuarios_serializer.data)
+    
+    elif request.method == "POST":
+        print (request.data)
--- Tienda/usuarios/api/urls.py
+++ Tienda/usuarios/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from usuarios.api.api import UsuarioAPIView
+from usuarios.api.api import usuario_api_view
 
 urlpatterns = [
-    path ("usuario/", UsuarioAPIView.as_view(), name = "usuario_api")
+    #   .as_view() se usa para activar una clase
+    # path ("usuario/", UsuarioAPIView.as_view(), name = "usuario_api"),
+    path ("usuario/", usuario_api_view, name = "usuario_api"),
 ]
