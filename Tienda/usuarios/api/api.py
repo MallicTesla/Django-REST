113bcb2df1a3cae8117905885a75ac615a619952
Mallic Tesla <el_mallic@hotmail.com>
2023-09-15 16:50:57 -0300

    actualisasion y borrado de usuario

--- Tienda/usuarios/api/api.py
+++ Tienda/usuarios/api/api.py
@@ -1,4 +1,5 @@
 # este archivo rempasa al views
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -16,9 +17,9 @@ from usuarios.api.serializers import UsuarioSerializers
 #         return Response (usuarios_serializer.data)
 
 #   esto hace lo mismo que la clase UsuarioAPIView
-#   al decorador se le pasa que metodos se le van a permiter que tenga la funcion
+#   al decorador se le pasa los metodos que van a ser usados por la funcion
 @api_view (["GET","POST"])
-def usuario_api_view (request):
+def usuarios_api_view (request:Request):
     if request.method == "GET":
         usuarios=Usuario.objects.all ()
         #   cuando queres serealizar un listado tenes que agregarle (many = True) para que sepa que es mas de uno
@@ -35,3 +36,34 @@ def usuario_api_view (request):
             return Response (usuarios_serializer.data)
         
         return Response (usuarios_serializer.errors)
+
+@api_view (["GET", "PUT", "DELETE"])
+def usuario_api_view (request:Request, id):
+    #   selecsiona a un usuario por id
+    if request.method == "GET":
+        #   esto es igual que usar objects.get (id=id)
+        usuario = Usuario.objects.filter(id = id ).first()
+        usuario_seria = UsuarioSerializers (usuario)
+
+        return Response (usuario_seria.data)
+
+    #   para editar un usuario
+    elif request.method == "PUT":
+        usuario = Usuario.objects.filter(id = id ).first()
+        #   la informasion de la actualisasion se guarda en (data=request.data)
+        usuario_seria = UsuarioSerializers(usuario, data=request.data)
+
+        if usuario_seria.is_valid():
+            usuario_seria.save()
+
+            return Response(usuario_seria.data)
+
+        return Response(usuario_seria.errors)
+    
+    elif request.method == "DELETE":
+        usuario = Usuario.objects.filter(id = id ).first()
+        usuario.delete()
+
+        return Response ("Eliminado")
+
+
--- Tienda/usuarios/api/urls.py
+++ Tienda/usuarios/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from usuarios.api.api import usuario_api_view
+from usuarios.api.api import usuarios_api_view, usuario_api_view
 
 urlpatterns = [
     #   .as_view() se usa para activar una clase
-    # path ("usuario/", UsuarioAPIView.as_view(), name = "usuario_api"),
-    path ("usuario/", usuario_api_view, name = "usuario_api"),
+    # path ("usuarios/", UsuarioAPIView.as_view(), name = "usuario_api"),
+    path ("usuarios/", usuarios_api_view, name = "usuarios_api"),
+    path ("usuario/<int:id>/", usuario_api_view, name = "usuario_api_view")
 ]
